6e2feed4fb4d0b3348cd41566be1bb9dfa399ac4
Ming <ming230614@gmail.com>
2026-02-12 22:16:02 +0000

    Add backend implementation

---
+++ Labs/Week 20 Lab/backend/mingdemo/src/test/java/com/example/mingdemo/MingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
